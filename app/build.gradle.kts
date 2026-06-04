729a7fe8fd812f145a0690b341a61969287ea5f4
Diego Farid Solorzano Villegas <PT72787033@idat.pe>
2026-06-04 15:36:26 -0500

    Se agregó vistas para registro e inicio de sesión

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.proyectomovil"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.proyectomovil"
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation ("com.google.android.material:material:1.12.0")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
