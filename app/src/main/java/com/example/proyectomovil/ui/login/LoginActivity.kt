5540198c7e966df1f45ac47887402bf7b2d17c88
roBvert <robertnicolassoto@gmail.com>
2026-06-04 21:42:24 -0500

    Vista Login corregida Vista Menu corregida Falta conectar el boton ingresar hacia el menu

--- app/src/main/java/com/example/proyectomovil/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/proyectomovil/ui/login/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.proyectomovil.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectomovil.MainActivity
 import com.example.proyectomovil.R
 
 class LoginActivity : AppCompatActivity() {
@@ -12,10 +16,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val tvRegistro = findViewById<TextView>(R.id.tvRegistro)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+
     }
 }
