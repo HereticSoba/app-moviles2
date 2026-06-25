0ef8065375c21e49356aa35efee989aa700f5c3b
felyp244 <felipe.yan12@gmail.com>
2026-06-25 17:27:27 -0500

    Cambio de version del glide a 5.0 y algo , antes estaba con el 4.x , Actividad favoritos hecho.

---
+++ app/src/main/java/com/example/proyectomovil/adapters/FavoritosAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.proyectomovil.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.proyectomovil.R
+import com.example.proyectomovil.entity.Pelicula
+import org.w3c.dom.Text
+
+class FavoritosAdapter(private val context : Context, private val lista : List<Pelicula>) : RecyclerView.Adapter<FavoritosAdapter.FavoritosViewholder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FavoritosViewholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pelicula,parent,false)
+        return FavoritosViewholder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: FavoritosViewholder,
+        position: Int
+    ) {
+        val pelicula =lista[position]
+        Glide.with(context).load(pelicula.image).into(holder.ivimage)
+        holder.tvtitulo.text = pelicula.title
+        holder.tvestreno.text = pelicula.anioEstreno.toString()
+        holder.tvduracion.text = pelicula.duracionMinutos.toString()
+        holder.tvcalificacion.text = pelicula.calificacion.toString()
+        holder.tvcategoria.text = pelicula.categoria
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+    inner class FavoritosViewholder(itemview : View) : RecyclerView.ViewHolder(itemview){
+        val ivimage : ImageView =itemview.findViewById<ImageView>(R.id.ivimagen)
+        val tvtitulo : TextView = itemview.findViewById<TextView>(R.id.tvTitulo)
+        val tvestreno : TextView = itemview.findViewById<TextView>(R.id.tvestreno)
+        val tvduracion : TextView = itemview.findViewById<TextView>(R.id.tvduracion)
+        val tvcalificacion : TextView = itemview.findViewById<TextView>(R.id.tvcalificacion)
+        val tvcategoria : TextView = itemview.findViewById<TextView>(R.id.tvcategoria)
+    }
+
+
+}
